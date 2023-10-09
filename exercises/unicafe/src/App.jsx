23f04f79ad028c62db5f0734e51fc70702832382
Ngo Xuan Huy <ngoxuanhuy@Ngos-MacBook-Pro.local>
2023-10-09 19:50:22 +0300

    Add Ex1.6: unicafe, step1

---
+++ exercises/unicafe/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  // onClick functions
+  const goodClick = () => setGood(good+1)
+  const neutralClick = () => setNeutral(neutral+1)
+  const badClick = () => setBad(bad+1)
+
+  return (
+    <>
+      <h1>Give Feedback</h1>
+      <button onClick={goodClick}>good</button>
+      <button onClick={neutralClick}>neutral</button>
+      <button onClick={badClick}>bad</button>
+      <h1>Statistics</h1>
+      <div>Good: {good}</div>
+      <div>Neutral: {neutral}</div>
+      <div>Bad: {bad}</div>
+    </>
+  )
+}
+
+export default App
