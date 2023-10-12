6593381626692bb78d82850b07fb3ad23d188e73
Ngo Xuan Huy <ngoxuanhuy@Ngos-MacBook-Pro.local>
2023-10-12 22:50:49 +0300

    Add Ex2.4: courseinfo, step9

--- exercises/courseinfo/src/components/Total.jsx
+++ exercises/courseinfo/src/components/Total.jsx
@@ -1,7 +1,7 @@
 const Total = ({parts}) => {
     const sum = parts.reduce((accumulator, currentValue) => accumulator + currentValue.exercises, 0)
     
-    return <div> Total of {sum} exercises </div>
+    return <div><strong>Total of {sum} exercises </strong></div>
 }
 
 export default Total
