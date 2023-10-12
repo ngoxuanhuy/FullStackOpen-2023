6a24450c7bd6919f8c355f8a24a01066b49e76c2
Ngo Xuan Huy <ngoxuanhuy@Ngos-MacBook-Pro.local>
2023-10-12 22:05:11 +0300

    Add Ex2.1: courseinfo, step6

--- exercises/courseinfo/src/App.jsx
+++ exercises/courseinfo/src/App.jsx
@@ -1,26 +1,4 @@
-const Header = (props) => {
-  return (
-    <h1>{props.name}</h1>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part.name} {props.part.exercises}
-    </p>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <div>
-      <Part part={props.parts[0]}/>
-      <Part part={props.parts[1]}/>
-      <Part part={props.parts[2]}/>
-    </div>
-  )
-}
+import Course from './components/Course'
 
 const Total = (props) => {
   return (
@@ -30,30 +8,33 @@ const Total = (props) => {
 
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'Something new is added',
+        exercises: 100,
+        id: 4
       }
     ]
   }
 
-  return (
-    <div>
-      <Header name={course.name}/>
-      <Content parts={course.parts}/>
-      <Total parts={course.parts}/>
-    </div>
-  )
+  return <Course course={course} />
 }
 
 export default App
